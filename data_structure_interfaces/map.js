ed6fc6900079a5d5c22ad0449a07a5969cb30c98
raberin <rmvaberin@gmail.com>
2019-11-16 19:20:38 -0800

    Map added

---
+++ data_structure_interfaces/map.js
@@ -0,0 +1,12 @@
+/*
+
+Very similar to JS Objects, BUT they don't have prototypes, 
+inheritance, methods, or anything of that sort.
+
+No concept of order as wll.
+
+Keys are all unique, BUT NOT VALUES
+Obj.x = 'map'
+Obj.y = 'map' 
+THIS IS OK
+*/
